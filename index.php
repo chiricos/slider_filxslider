34be8631d71a70615e0954cb91a773cd6db0b95d
drawde <drawde@iMac-de-Web-2.local>
2015-08-06 09:54:18 -0500

    aprendiendo a colocar slider genericos

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>slider</title>
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <script src="js/jquery.flexslider.js"></script>
+     <script src="js/script.js"></script>
+</head>
+<body>
+<!-- Place somewhere in the <body> of your page -->
+<div class="flexslider">
+  <ul class="slides">
+    <li>
+      <img src="images/slide/1.jpg" />
+    </li>
+    <li>
+      <img src="images/slide/2.jpg" />
+    </li>
+    <li>
+      <img src="images/slide/3.jpg" />
+    </li>
+  </ul>
+</div>
+  
+</ul>
+</body>
+</html>
